89c8896ad3e0e605c6d2d335e745d72951c6ad99
rjvpersaud@gmail.com <rjvpersaud@gmail.com>
2023-09-29 14:07:04 +0000

    sql done

--- models/customers2.sql
+++ models/customers2.sql
@@ -1,22 +1,13 @@
 with tmp_customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
+    select * from {{ ref('stg_customers') }}
 
-    from sandbox01.dbtcloud.customers
 ),
 
 tmp_orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
+    select * from {{ ref('stg_orders') }}
 
-    from sandbox01.dbtcloud.shop_orders
 ),
 
 
