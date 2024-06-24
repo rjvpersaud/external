a261824a202261e0fafcba5a04272faf6c0dc9f2
rjvpersaud <56870751+rjvpersaud@users.noreply.github.com>
2024-06-24 18:01:22 +0100

    Create tbl_info.sql

---
+++ trino-explain/tbl_info.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE VIEW imp.hrs_vw.eval91_tbl_info as
+with tbl as ( SELECT e.query_id , e.tbl,e.scntyp,e.ord, frgno
+    from
+      -- Table
+      SANDBOX01.exp.scan e
+      -- Fragment
+      left join SANDBOX01.exp.frag on frag.query_id = e.query_id
+      and e.ord between frag.ord and frag.end_ord
+      where indent =4 and frgtyp = 'SOURCE') 
+, est as(
+select frag.query_id , frag.frgno , e.ord , missing , size_data_val , size_data_unt from 
+SANDBOX01.exp.estimates e 
+      left join SANDBOX01.exp.frag on frag.query_id = e.query_id
+      and e.ord between frag.ord and frag.end_ord
+where frgtyp = 'SOURCE')
+select tbl.query_id
+,tbl
+,scntyp
+,tbl.frgno
+,missing
+,size_data_val
+,size_data_unt
+from est,tbl
+where est.query_id = tbl.query_id
+  and est.frgno = tbl.frgno;
