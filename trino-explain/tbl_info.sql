d804db5e962b03654b816c5288f3296c4b455054
rjvpersaud <56870751+rjvpersaud@users.noreply.github.com>
2024-06-25 12:14:46 +0100

    Update tbl_info.sql

--- trino-explain/tbl_info.sql
+++ trino-explain/tbl_info.sql
@@ -11,7 +11,7 @@ with tbl as ( SELECT e.query_id , e.tbl,e.scntyp,e.ord, frgno
 , est as(
 select frag.query_id , frag.frgno , e.ord , missing , size_data_val , size_data_unt from 
 estimates e 
-      left join SANDBOX01.exp.frag on frag.query_id = e.query_id
+      left join frag on frag.query_id = e.query_id
       and e.ord between frag.ord and frag.end_ord
 where frgtyp = 'SOURCE')
 select tbl.query_id
